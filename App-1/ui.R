b0549e2b538fb34a52e04922d6f0f6982804f946
chiu <vincent.chiu@enernoc.com>
2015-11-20 16:10:19 -0800

    star wars now

--- App-1/ui.R
+++ App-1/ui.R
@@ -1,14 +1,17 @@
 # ui.R
 
 shinyUI(fluidPage(
-  titlePanel("cool wow title panel"),
-  sidebarLayout(sidebarPanel("sidebar page panel"),
-                mainPanel(
-                  h1("First level title"),
-                  h2("SEcond level title"),
-                  h3("Third level title"),
-                  h4("Fourth level title"),
-                  h5("Fifth level title"),
-                  h6("Sixth level title")
-                ))
+  titlePanel("Star Wars is Cool"),
+  sidebarLayout(
+    sidebarPanel("chewbacca"),
+    mainPanel(
+      h6("Episode IV", align = "center"),
+      h6("A NEW HOPE", align = "center"),
+      h5("It is a period of civil war.", align = "center"),
+      h4("Rebel spaceships, striking", align = "center"),
+      h3("from a hidden base, have won", align = "center"),
+      h2("their first victory against the", align = "center"),
+      h1("evil Galactic Empire.")
+    )
+  )
 ))
