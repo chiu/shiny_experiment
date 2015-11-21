796ec8a6339cad0bf8a4e366e33d7f22ab30271b
chiu <vincent.chiu@enernoc.com>
2015-11-20 16:08:39 -0800

    progressively smaller text

--- App-1/ui.R
+++ App-1/ui.R
@@ -2,13 +2,13 @@
 
 shinyUI(fluidPage(
   titlePanel("cool wow title panel"),
-  
-  sidebarLayout(
-    sidebarPanel( "sidebar page panel"),
-    mainPanel("main page panel")
-    
-    
-  )
-  
-  
+  sidebarLayout(sidebarPanel("sidebar page panel"),
+                mainPanel(
+                  h1("First level title"),
+                  h2("SEcond level title"),
+                  h3("Third level title"),
+                  h4("Fourth level title"),
+                  h5("Fifth level title"),
+                  h6("Sixth level title")
+                ))
 ))
