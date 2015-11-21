75a105956dfce19f078a0a05f456234e236cf553
chiu <vincent.chiu@enernoc.com>
2015-11-20 16:30:01 -0800

    almost done styling

--- App-1/ui.R
+++ App-1/ui.R
@@ -3,19 +3,42 @@
 shinyUI(fluidPage(
   titlePanel("My Shiny App"),
   sidebarLayout(
-    
     sidebarPanel(
       h1("Installation"),
       p("Shiny is available in CRAN..."),
       code("install.pack..."),
       br(),
-      img(src="bigorb.png", height = 40, width = 40),
-      p("shiny is a product of"),
-      a(href="www.rstudio.com", "Rstudio")
+      img(
+        src = "bigorb.png",
+        height = 40,
+        width = 40
+      ),
+      div("shiny is a product of"),
+      a(href = "https://www.rstudio.com", "Rstudio")
     ),
     mainPanel(
       h1("Introducing Shiny"),
-      p("Shiny something somethings")
+      p("Shiny something somethings"),
+      
+      
+      br(),
+      h2("Features"),
+      p("some cool stuff"),
+      tags$ul(
+        tags$li("First list item"),
+        tags$li(
+          HTML(paste("E = mc", tags$sup(2), sep = "")), 
+          HTML(paste("H", tags$sub(2), "0", sep = "")),
+          HTML(paste("something ", tags$b("bold text "), sep = "", "so cool"))
+          
+          ),
+        tags$li("Third list item")
+      )
+      ## <ol>
+      ##   <li>First list item</li>
+      ##   <li>Second list item</li>
+      ##   <li>Third list item</li>
+      ## </ol>
     )
   )
 ))
