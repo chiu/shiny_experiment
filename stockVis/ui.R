6d02e21837edcd7476a5a12dbc567ed7e5781d5e
chiu <chiucode@gmail.com>
2015-11-23 15:42:12 -0800

    reformatted code

--- stockVis/ui.R
+++ stockVis/ui.R
@@ -5,25 +5,29 @@ shinyUI(fluidPage(
   
   sidebarLayout(
     sidebarPanel(
-      helpText("Select a stock to examine. 
-        Information will be collected from yahoo finance."),
-    
+      helpText(
+        "Select a stock to examine.
+        Information will be collected from yahoo finance."
+      ),
+      
       textInput("symb", "Symbol", "SPY"),
-    
-      dateRangeInput("dates", 
+      
+      dateRangeInput(
+        "dates",
         "Date range",
-        start = "2013-01-01", 
-        end = as.character(Sys.Date())),
+        start = "2013-01-01",
+        end = as.character(Sys.Date())
+      ),
       
       br(),
       br(),
       
-      checkboxInput("log", "Plot y axis on log scale", 
-        value = FALSE),
+      checkboxInput("log", "Plot y axis on log scale",
+                    value = FALSE),
       
-      checkboxInput("adjust", 
-        "Adjust prices for inflation", value = FALSE)
-    ),
+      checkboxInput("adjust",
+                    "Adjust prices for inflation", value = FALSE)
+      ),
     
     mainPanel(plotOutput("plot"))
   )
