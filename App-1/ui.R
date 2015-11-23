6596fca28c1d98629e68a71af3d44b655d9550ed
chiu <vincent.chiu@enernoc.com>
2015-11-23 10:41:36 -0800

    got general idea down

--- App-1/ui.R
+++ App-1/ui.R
@@ -2,23 +2,43 @@
 
 shinyUI(fluidPage(
   titlePanel("censusVis"),
+  sidebarLayout(
+  sidebarPanel(
+  selectInput(
+      "select",
+      label = h3("Select box"),
+      choices = list(
+        "Choice wowee" = 77,
+        "Choice 2" = 2,
+        "Choice 3" = 3
+      ),
+      selected = 2
+    ),
+                      
+                      
+      sliderInput(
+        "slider1",
+        label = h3("Sliders"),
+        min = 0,
+        max = 200,
+        value = 60
+      ),
+      sliderInput(
+        "slider2",
+        "",
+        min = 0,
+        max = 100,
+        value = c(25, 75)
+      )
   
+      ),
+  mainPanel(
+    h1("")
+  )
+  
+    )
+  
+)
 
-  fluidRow(
-    
-    
-    column(3,
-           selectInput("select", label = h3("Select box"), 
-                       choices = list("Choice wowee" = 77, "Choice 2" = 2,
-                                      "Choice 3" = 3), selected = 2)),
-    
-    column(3, 
-           sliderInput("slider1", label = h3("Sliders"),
-                       min = 0, max = 200, value = 60),
-           sliderInput("slider2", "",
-                       min = 0, max = 100, value = c(25, 75))
-    ))
-    
-  
-  
-))
+)
+        
